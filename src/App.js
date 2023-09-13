6c76fa37ddb34fee4ddd5cc64622077cf1e1d8b4
sonji406 <wkaqh24@gmail.com>
2023-09-13 20:27:33 +0900

    Feat: 사이드바 생성

--- src/App.js
+++ src/App.js
@@ -5,16 +5,39 @@ import CreateHabit from './pages/CreateHabitPage';
 import MyHabit from './pages/MyHabitPage';
 import GroupPage from './pages/GroupPage';
 import AwsS3Test from './pages/AwsS3Test';
+import Sidebar from './components/Common/Sidebar';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/my-habit/:nickname/new-habit' element={<CreateHabit />} />
-        <Route path='/my-habit/:nickname' element={<MyHabit />} />
-        <Route path='/group/:groupId' element={<GroupPage />} />
-        <Route path='/aws-s3-test' element={<AwsS3Test />} />
+        <Route
+          path='/'
+          element={
+            <>
+              <HomePage />
+            </>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <div className='flex'>
+              <Sidebar />
+              <div className='flex-1'>
+                <Routes>
+                  <Route
+                    path='/my-habit/:nickname/new-habit'
+                    element={<CreateHabit />}
+                  />
+                  <Route path='/my-habit/:nickname' element={<MyHabit />} />
+                  <Route path='/group/:groupId' element={<GroupPage />} />
+                  <Route path='/aws-s3-test' element={<AwsS3Test />} />
+                </Routes>
+              </div>
+            </div>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ src/components/Common/Sidebar.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import HamburgerButton from './HamburgerButton';
+import MyHabitPageButton from './MyHabitPageButton';
+import CreateGroupButton from './CreateGroupButton';
+import JoinedGroupsButton from './JoinedGroupsButton';
+
+const Sidebar = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    console.log('true');
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    console.log('false');
+  };
+
+  return (
+    <div
+      className={`h-screen sticky top-0 bg-[#2D3C4A] shadow-md transition-all ${
+        isHovered ? 'w-64' : 'w-20'
+      }`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={`flex flex-col ${
+          isHovered ? 'items-end pr-6' : 'items-center'
+        }`}
+      >
+        <HamburgerButton isHovered={isHovered} />
+      </div>
+      <div
+        className={`flex flex-col space-y-14 ${
+          isHovered ? 'items-start w-64 pl-6' : 'items-center'
+        } transition-all`}
+      >
+        <MyHabitPageButton isHovered={isHovered} />
+        <CreateGroupButton isHovered={isHovered} />
+        <JoinedGroupsButton isHovered={isHovered} />
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
