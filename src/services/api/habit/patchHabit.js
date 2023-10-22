dbb7fbb4818e8dd7bbb888c45646f7e3c4f391b4
sonji406 <wkaqh24@gmail.com>
2023-10-23 02:03:39 +0900

    Fix: patchHabitAPI 비동기 작업

--- src/services/api/habit/patchHabit.js
+++ src/services/api/habit/patchHabit.js
@@ -1,7 +1,9 @@
 import api from '../../../lib/api';
 
-const patchHabitAPI = (habitId, habitData) => {
-  return api.patch(`/habit/${habitId}`, habitData, { withCredentials: true });
+const patchHabitAPI = async (habitId, habitData) => {
+  return await api.patch(`/habit/${habitId}`, habitData, {
+    withCredentials: true,
+  });
 };
 
 export default patchHabitAPI;
