a8cac7158e96a0ae10a9ffc6659e8c9a927cd8f6
sonji406 <wkaqh24@gmail.com>
2023-10-23 02:02:30 +0900

    Fix: getGroupAPI 비동기 작업 및 반환 값 수정

--- src/services/api/group/getGroup.js
+++ src/services/api/group/getGroup.js
@@ -4,11 +4,9 @@ import api from '../../../lib/api';
 const getGroupAPI = async (groupId) => {
   const userId = getUserIdFromToken();
 
-  const response = await api.get(`/group/${groupId}?userId=${userId}`, {
+  return await api.get(`/group/${groupId}?userId=${userId}`, {
     withCredentials: true,
   });
-
-  return response.data;
 };
 
 export default getGroupAPI;
