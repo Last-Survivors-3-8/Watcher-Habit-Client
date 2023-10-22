fec15c4ece42abc1a011e0faf898738d3f844c2a
sonji406 <wkaqh24@gmail.com>
2023-10-23 02:05:33 +0900

    Fix: postRefreshTokenAPI 비동기 작업

--- src/services/api/auth/postRefreshToken.js
+++ src/services/api/auth/postRefreshToken.js
@@ -4,8 +4,8 @@ const refreshTokenAPI = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_DOMAIN}/api`,
 });
 
-const postRefreshTokenAPI = () => {
-  return refreshTokenAPI.post(
+const postRefreshTokenAPI = async () => {
+  return await refreshTokenAPI.post(
     '/auth/refreshToken',
     {},
     { withCredentials: true },
