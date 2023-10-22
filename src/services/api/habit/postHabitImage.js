6b279315b3982ef623b1928ec75ebb43c55f86b6
sonji406 <wkaqh24@gmail.com>
2023-10-23 02:02:53 +0900

    Fix: postHabitImageAPI 비동기 작업

--- src/services/api/habit/postHabitImage.js
+++ src/services/api/habit/postHabitImage.js
@@ -1,7 +1,7 @@
 import api from '../../../lib/api';
 
-const postHabitImageAPI = (habitId, body) => {
-  return api.post(`/habit/${habitId}/image`, body, {
+const postHabitImageAPI = async (habitId, body) => {
+  return await api.post(`/habit/${habitId}/image`, body, {
     headers: { 'Content-Type': 'multipart/form-data' },
     withCredentials: true,
   });
